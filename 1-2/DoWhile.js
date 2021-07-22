6018c5c11d29cddcb8f39f30a63cc21b24fcf37b
Sanzaka <equation0815@icloud.com>
2021-07-22 16:20:25 +0900

    [end]1-2

---
+++ 1-2/DoWhile.js
@@ -0,0 +1,10 @@
+let ticket = 3;
+let play = 0;
+
+ticket - 1;
+do {
+  play = play + 1;
+  alert(play+ "回遊びました");
+}while(play < 3);
+
+alert("3回遊んだため終了です。")
---
+++ 1-2/app.js
@@ -0,0 +1,15 @@
+let int1 = 1;
+let int2 = -10;
+let float = 3.14;
+
+let str1 = "JSを覚えよう";
+
+alert(4 + 3)
+alert(3 * 5)
+
+alert("Hello" + "World")
+
+let str2 = "Hello"
+let str3 = "World!!"
+alert(str1 + str2);
+

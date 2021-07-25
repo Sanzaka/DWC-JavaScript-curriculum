996f2d4e6f0c39898516e0899d4cef3fbfc643ea
Sanzaka <equation0815@icloud.com>
2021-07-25 14:18:05 +0900

    [end]1-10,ハンバーガーメニュー

--- SlideShow-Lesson/js/script.js
+++ SlideShow-Lesson/js/script.js
@@ -13,9 +13,9 @@ $(".theTarget").skippr({
   //ナビゲーション矢印の表示(trueで表示)
   arrows : true,
   //スライドショーの自動再生(falseで自動再生なし)
-  autoPlay : false,
+  autoPlay : true,
   //自動再生時のスライド切り替え間隔 (ミリ秒)
-  autoPlayDuration : 5000,
+  autoPlayDuration : 2000,
   //キーボードの矢印キーによるスライド送りの設定(trueで有効)
   keyboardOnAlways : true,
   //一枚目のスライド表示時に戻る矢印を表示するかどうか[false]:矢印を隠さない,[true]:矢印を隠す
